f84ea7fdd78d146d889ce62709c38a915f421fa1
xjf255 <juanfher.255@gmail.com>
2023-10-04 12:32:38 -0600

    last change to docs

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-const SUBMIT = document.querySelector('#submit')
-const MESSAGESUCCESS = document.getElementsByClassName('messageSuccess--none')
-const FORM_MOBILE =document.getElementsByClassName('formSignUp__mobile')
-const FORM_DESKTOP =document.getElementsByClassName('formSignUp__desktop')
-let form = [...FORM_MOBILE]
+const $submit = document.querySelector('#submit')
+const $submitDesktop = document.querySelector('#submitDesktop')
+const $messageSuccess = document.getElementById('messageSuccess')
+const $formMobile =document.getElementById('formSignUp__mobile')
+const $formDesktop =document.getElementById('formSignUp__desktop')
+const $emailMobile = document.getElementById("emailMobile")
+const $emailDesktop = document.getElementById("emailDesktop")
 
 
-SUBMIT.addEventListener('click',function (){
-    console.log(document.documentElement.clientWidth)
-    // FORM_MOBILE.toogle('formSignUp_mobile--none')
-    // MESSAGESUCCESS.
+function obtenerEmail(mail){
+    const email = mail.value;
+    const validarEmail = email.split("@");
+    last = validarEmail.length-1;
+    return `@${validarEmail[last]}`
+}
+
+const gmail="@loremcompany.com";
+
+
+$submit.addEventListener('click',function (){
+    if(obtenerEmail($emailMobile) === gmail){
+        $formMobile.classList.toggle("formSignUp__mobile--none")
+        $formDesktop.classList.toggle("formSignUp__desktop--none")
+        $messageSuccess.classList.remove("messageSuccess--none")
+        $messageSuccess.classList.add("messageSuccess")
+    }else{
+        alert("La cagaste");
+    }   
+})
+$submitDesktop.addEventListener('click',function (){
+    if(obtenerEmail($emailDesktop) === gmail){
+        $formMobile.classList.toggle("formSignUp__mobile--none")
+        $formDesktop.classList.toggle("formSignUp__desktop--none")
+        console.log($formDesktop)
+        $messageSuccess.classList.remove("messageSuccess--none")
+        $messageSuccess.classList.add("messageSuccess")
+    }else{
+        alert("La cagaste");
+    }
 })
